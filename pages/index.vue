7fd025cb092a16fb8585486105431895004ca7e1
Ellanski <nameimprobable@gmail.com>
2019-10-17 18:30:01 +0300

    another test

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
   <section class="section">
     <div class="columns is-mobile">
+      <nuxt-link to="users/1">user 1</nuxt-link>
       <nuxt-link to="users/2">user 2</nuxt-link>
     </div>
   </section>
